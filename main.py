58d241a17ad9bebf10fe37ca08ba815f565e448d
Asyajj Abdul Qais <me@example.com>
2026-06-07 21:02:36 +0700

    fix : bug main.py

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ DEFAULT_CORS_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://tumbangid.vercel.app",
 ]
 
 cors_origins_env = os.getenv("CORS_ORIGINS")
